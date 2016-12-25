d35999f13ad58734400e47595fa0a2a0fe2f4af2
curious725 <curious725@gmail.com>
2016-12-25 17:22:07 +0200

    add page layout

--- studentsdb/studentsdb/students/views.py
+++ studentsdb/studentsdb/students/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Views for Students
+
+
+def students_list(request):
+    return render(
+        request, 'students/students_list.html', {}
+    )
+
+
+def students_add(request):
+    return HttpResponse(
+        '<h1>Student Add Form</h1>'
+    )
+
+
+def students_edit(request, sid):
+    return HttpResponse(
+        '<h1>Edit Student {} </h1>'.format(sid)
+    )
+
+
+def students_delete(request, sid):
+    return HttpResponse(
+        '<h1>Delete Student {} </h1>'.format(sid)
+    )
+
+# Views for Groups
+
+
+def groups_list(request):
+    return HttpResponse(
+        '<h1>Groups Listing</h1>'
+    )
+
+
+def groups_add(request):
+    return HttpResponse(
+        '<h1>Group Add Form</h1>'
+    )
+
+
+def groups_edit(request, gid):
+    return HttpResponse(
+        '<h1>Edit Group {} </h1>'.format(gid)
+    )
+
+
+def groups_delete(request, gid):
+    return HttpResponse(
+        '<h1>Delete Group {} </h1>'.format(gid)
+    )
