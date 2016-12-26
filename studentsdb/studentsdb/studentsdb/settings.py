aefd7bf1cf6840420f80d5e53ec9f2282a929c81
curious725 <curious725@gmail.com>
2016-12-26 12:39:43 +0200

    make custom absolute path context processor

--- studentsdb/studentsdb/studentsdb/settings.py
+++ studentsdb/studentsdb/studentsdb/settings.py
@@ -13,6 +13,7 @@ from django.conf import global_settings
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+PORTAL_URL = 'http://localhost:8000'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -88,4 +89,6 @@ STATIC_URL = '/static/'
 TEMPLATE_CONTEXT_PROCESSORS = \
     global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
         "django.core.context_processors.request",
+        "studentsdb.context_processors.students_proc"
     )
+
