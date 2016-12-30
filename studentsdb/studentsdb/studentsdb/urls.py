11a8d1349b3aa20f5dd27a95d439522509daf6c3
curious725 <curious725@gmail.com>
2016-12-30 14:03:13 +0200

    modify urls.py to support media folder

--- studentsdb/studentsdb/studentsdb/urls.py
+++ studentsdb/studentsdb/studentsdb/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
@@ -28,3 +31,9 @@ urlpatterns = patterns(
         name='groups_delete')
 
 )
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
