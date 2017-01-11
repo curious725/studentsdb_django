56603e335ba3f296d24f47258dc41fd44c1f89f8
curious725 <curious725@gmail.com>
2017-01-11 15:34:05 +0200

    add logic to add student view

--- studentsdb/studentsdb/students/views/students.py
+++ studentsdb/studentsdb/students/views/students.py
@@ -2,7 +2,8 @@
 from ..models import Student, Group
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -39,6 +40,51 @@ def students_list(request):
 
 
 def students_add(request):
+    # Якщо форма була запощена:
+    if request.method == "POST":
+        # Якщо кнопка скасувати була натиснута:
+        if request.POST.get('cancel_button') is not None:
+            # Повертаємо користувача до списку студентів
+            return HttpResponseRedirect(reverse('home'))
+
+        # Якщо кнопка додати була натиснута:
+        if request.POST.get('add_button') is not None:
+
+            # Перевіряємо дані на коректність та збираємо помилки
+            errors = {}
+
+            # Якщо дані були введені коректно:
+            if not errors:
+                # Створюємо студента
+                student = Student(
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    middle_name=request.POST['middle_name'],
+                    birthday=request.POST['birthday'],
+                    ticket=request.POST['ticket'],
+                    student_group=Group.objects.get(
+                        pk=request.POST['student_group']
+                    ),
+                    photo=request.FILES['photo'],
+                    notes=request.POST['notes'],
+                )
+                # Зберігаємо студента в базу
+                student.save()
+                # Повертаємо користувача до списку студентів
+                return HttpResponseRedirect(reverse('home'))
+
+            # Якщо дані були введені некоректно:
+            else:
+                # Віддаємо шаблон форми разом зі знайденими помилками та
+                # попередніми введеними даними користувача
+                return render(
+                    request, 'students/students_add.html',
+                    {'groups': Group.objects.all().order_by('title'),
+                     'errors': errors}
+                )
+
+    # Якщо форма не була запощена:
+    # повертаємо код початкового стану форми
     return render(
         request, 'students/students_add.html',
         {'groups': Group.objects.all().order_by('title')}
