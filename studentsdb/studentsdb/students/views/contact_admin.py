18f6e39216605b237e9ce2a970784ff81820cbad
curious725 <curious725@gmail.com>
2017-01-13 12:49:29 +0200

    contact admin view

--- studentsdb/studentsdb/students/views/contact_admin.py
+++ studentsdb/studentsdb/students/views/contact_admin.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django import forms
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from studentsdb.settings import ADMIN_EMAIL
 
 
 class ContactForm(forms.Form):
@@ -20,6 +26,35 @@ class ContactForm(forms.Form):
 
 
 def contact_admin(request):
+    # check if form was posted
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request
+        form = ContactForm(request.POST)
+
+        # check whether user data is valid:
+        if form.is_valid():
+            # send email
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            from_email = form.cleaned_data['from_email']
+
+            try:
+                send_mail(subject, message, from_email, [ADMIN_EMAIL])
+            except Exception:
+                message = u"Під час відправки листа виникла непередбачувана "\
+                    u"помилка.Спробуйте скористатися даною формою пізніше."
+            else:
+                message = u"Повідомлення успішно надіслане!"
+
+            # redirect to same contact page with success message
+            return HttpResponseRedirect(
+                u"{}?status_message={}".format(
+                    reverse('contact_admin'), message
+                )
+            )
+    # if there was not POST render blank form
+    else:
+        form = ContactForm()
     return render(
-        request, 'contact_admin/form.html', {}
+        request, 'contact_admin/form.html', {'form': form}
     )
