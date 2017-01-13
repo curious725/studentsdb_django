3a1cebd5fbbf874a1a3e6ae3ecdbd53fdacc608d
curious725 <curious725@gmail.com>
2017-01-13 13:50:56 +0200

    add django crispy forms to the list of installed apps

--- studentsdb/studentsdb/studentsdb/settings.py
+++ studentsdb/studentsdb/studentsdb/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'students'
+    'crispy_forms',
+    'students',
 )
 
 MIDDLEWARE_CLASSES = (
