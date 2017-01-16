4ced6a785f2dc8affe87432af192a59526b685cc
curious725 <curious725@gmail.com>
2017-01-16 13:41:27 +0200

    register Group, StudentAdmin in admin

--- studentsdb/studentsdb/students/admin.py
+++ studentsdb/studentsdb/students/admin.py
@@ -1,5 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
-from .models import Student
+
+from .models import Student, Group
+
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Group)
